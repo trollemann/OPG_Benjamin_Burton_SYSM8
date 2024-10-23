1908e59fe6720aed37c02e60676e590dd6cba211
benjamin burton <benjamin.alexander.burton@gmail.com>
2024-10-24 01:14:23 +0200

    fortsatt på ForgotPasswordWindow

--- Fit_Track/ViewModel/ForgotPasswordViewModel.cs
+++ Fit_Track/ViewModel/ForgotPasswordViewModel.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Fit_Track.ViewModel
+﻿namespace Fit_Track.ViewModel
 {
-    internal class ForgotPasswordViewModel
+    public class ForgotPasswordViewModel : ViewModelBase
     {
+        //properties
+        private string _username;
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
